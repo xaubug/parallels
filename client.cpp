a4e36e5f1ad23da64c1be5496f8ca8752f1cb13e
Maxim <32304530+xaubug@users.noreply.github.com>
2021-12-28 21:19:43 +0300

    Create client.cpp

---
+++ client.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Maxim Maximovsky on 28.12.2021.
+//
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include "mpi.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+    int lt_rem[2];
+    int l_indic;
+    int l_bet;
+    int l_min;
+    int l_rank;
+    int l_rem;
+    bool f_game_over;
+
+    lt_rem[0] = 20;
+
+    MPI_Init(&argc, &argv);
+    char port_name[MPI_MAX_PORT_NAME];
+    MPI_Comm intercomm;
+    MPI_Status status;
+    strcpy(port_name, argv[1]);
+    MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
+    MPI_Comm_rank(MPI_COMM_WORLD, &l_rank);
+
+
+    for(;;) {
+        cout << "У вас камней:" << lt_rem[0] << endl;
+        cout << "Введи: 0 - чётное, 1 - нечётное." << endl;
+        cin >> l_indic;
+        cout << "Загадай число" << endl;
+        cin >> l_bet;
+        MPI_Recv(&l_rem, 1, MPI_INT, 0, 0, intercomm, &status);
+
+        if (l_indic == (l_rem % 2)) {
+            cout << "Вы выигываете камней: " << min(l_bet, l_rem) << endl;
+            lt_rem[0] += min(l_bet, l_rem);
+            l_min =  - min(l_bet, l_rem);
+            MPI_Send(&l_min, 1, MPI_INT, 0, 0, intercomm);
+        }
+        else {
+            cout << "Ты теряешь камней: " << min(l_bet, l_rem) << endl;
+            lt_rem[0] -= min(l_bet, l_rem);
+            l_min = min(l_bet, l_rem);
+            MPI_Send(&l_min, 1, MPI_INT, 0, 0, intercomm);
+        }
+
+        f_game_over = false;
+
+        switch (lt_rem[0]) {
+            case 40: cout << "Ты выиграл" << endl;
+                f_game_over = true;
+                break;
+            case 0:  cout << "Тебе конец!" << endl;
+                f_game_over = true;
+                break;
+        }
+
+        if (f_game_over) break;
+
+    }
+    MPI_Finalize();
+    return 0;
+}
+
